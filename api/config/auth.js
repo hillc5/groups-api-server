24ab8f2dd30d4938951ff41064b068dc98e4c69b
charles5231 <charlieh@slalom.com>
2016-05-03 18:25:54 -0700

    Upping salt factor adding linting

--- api/config/auth.js
+++ api/config/auth.js
@@ -2,11 +2,11 @@ module.exports = {
     development: {
         signature: '0b83fc9e-71f3-4807-a3f2-fd7fd0cb7551',
         tokenExpires: 3600,
-        saltFactor: 13
+        saltFactor: 14
     },
     production: {
         signature: 'e997e569-3fd0-4c19-9c4c-690642d3edcc',
         tokenExpires: 36000,
-        saltFactor: 14
+        saltFactor: 15
     }
 };
